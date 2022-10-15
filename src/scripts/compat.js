1530064f6fc85af3faed296fc3c79facc679b423
Erik Golinelli <erik@codekraft.it>
2022-10-15 16:34:01 +0200

    compatibility stuff

---
+++ src/scripts/compat.js
@@ -0,0 +1,9 @@
+// detect available wheel event
+export const mouseWheel =
+	'onwheel' in document.createElement( 'div' )
+		? 'wheel' // Modern browsers support "wheel"
+		: document.onmousewheel !== undefined
+		? 'mousewheel' // Webkit and IE support at least "mousewheel"
+		: 'DOMMouseScroll'; // let's assume that remaining browsers are older Firefox// detect available wheel event
+
+export const hasTouch = 'ontouchstart' in window;
